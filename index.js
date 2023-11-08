df9d82b4d40ba1c4ac970adc9dd7dbb54ae56671
Replit user <>
2023-11-08 03:04:25 +0000

    complete

--- index.js
+++ index.js
@@ -30,3 +30,61 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+// /api/:date? requst
+app.get("/api/:date?", (req, res) => {
+  let date;
+  let unix;
+  let isUnix;
+  let utc;
+  let utcDate;
+  let dateObj;
+  let utcTest;
+  let testDate;
+  let testInt;
+  let unix1;
+  let intDate;
+
+  date = req.params.date;
+  
+  // check date for unix format
+  isUnix = /^\d+$/.test(date);
+  testDate = new Date(date);
+  utcTest = testDate.toUTCString();
+  
+if(isUnix) {
+  intDate = parseInt(date);
+  dateObj = new Date(intDate);
+  unix1 = dateObj.getTime();
+  unix = unix1;
+  utc = dateObj.toUTCString();
+} else {
+  dateObj =  new Date(date);
+  unix = dateObj.getTime();
+  utc = dateObj.toUTCString();
+}
+
+  console.log(date);
+  console.log(isUnix);
+  console.log(dateObj);
+  console.log(unix)
+  console.log(utc);
+  console.log("-------------------------")
+  
+  
+  if(date === undefined) {
+    let latestDate = new Date();
+    utc = latestDate.toUTCString();
+    unix = latestDate.getTime();
+    res.json({unix: unix, utc: utc});
+  };
+  
+  if(unix == null || utc == "Invalid Date") {
+    res.json({error: "Invalid Date"})
+  } else {
+    res.json({unix: unix, utc: utc});
+  }
+
+
+  });
+  
